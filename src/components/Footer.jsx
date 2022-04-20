21c554395632884028c25f4bdf430767e423f5a1
onestly <onestlyof@gmail.com>
2022-04-20 13:28:06 +0300

    add gh-pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ function Footer() {
 	<footer className="page-footer green lighten-3">
 		<div className="footer-copyright">
 			<div className="container">
-				© {new Date().getFullYear()} Copyright Text
-				<a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+				© {new Date().getFullYear()} oNestly
+				<a className="grey-text text-lighten-4 right" href="https://github.com/oNestly/shop-react-app" target="_blank" rel='noreferrer'>Repo</a>
 			</div>
 		</div>
 	</footer>
